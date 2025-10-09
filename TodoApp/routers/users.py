4d40ea0fb0c313c5d0093178f5434ee38fc12b03
Mayank Mehta <mayankmehta880@gmail.com>
2025-10-09 18:35:22 +0530

    add phone number field to user creation and implement phone number update functionality

--- TodoApp/routers/users.py
+++ TodoApp/routers/users.py
@@ -51,3 +51,14 @@ async def update_password(user_verfication:UserVerification,user:user_dependency
 
     db.add(user_model)
     db.commit()
+
+@router.put("/phonenumber/{phone_number}",status_code=status.HTTP_204_NO_CONTENT)
+async def update_phone_number(user:user_dependency,db:db_dependency,phone_number:str):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Authentication failed")
+    
+    user_model = db.query(Users).filter(Users.id==user.get("id")).first()
+    user_model.phone_number = phone_number
+
+    db.add(user_model)
+    db.commit()
